5e62536d7aacfeb49a7c2ba57f1e78d3d70e0e3e
Edward Song <eddie.y.song@gmail.com>
2024-05-12 17:32:03 -0400

    added a simple menu

--- Assets/BlockBehavior.cs
+++ Assets/BlockBehavior.cs
@@ -6,8 +6,8 @@ public class BlockBehavior : MonoBehaviour
 {
     public GameObject blockPrefab;
 
-    Rigidbody2D _rb2d;
-    int _blockSize = 0;
+    
+    public bool UntouchedBlock;
 
     public int BlockSize
     {
@@ -21,9 +21,17 @@ public class BlockBehavior : MonoBehaviour
         }
     }
 
+
+    GameManager gameManager;
+    Rigidbody2D _rb2d;
+    int _blockSize = 0;
+
+
     // Start is called before the first frame update
     void Start()
     {
+        gameManager = FindObjectOfType<GameManager>();
+
         //_blockSize = Random.Range(1, 3);
         var rotationAngle = Random.Range(-25, 25);
 
@@ -41,33 +49,39 @@ public class BlockBehavior : MonoBehaviour
     void SetBlockSize(int size = 1)
     {
         _blockSize = size;
-        Debug.Log("SetBlockSize");
-        Debug.Log(size);
+        //Debug.Log("SetBlockSize");
+        //Debug.Log(size);
         float sideLength = size * 0.2f + 0.2f;
         transform.localScale = new Vector3(sideLength, sideLength, 1);
     }
 
+
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log(collision);
-        Debug.Log(collision.collider.gameObject);
+        //Debug.Log(collision);
+        //Debug.Log(collision.collider.gameObject);
         var bb = collision.collider.gameObject.GetComponent<BlockBehavior>();
         if (bb != null)
         {
-            Debug.Log(bb.BlockSize);
-
+            //Debug.Log(bb.BlockSize);
             if (bb.BlockSize == BlockSize)
             {
                 bb.gameObject.SetActive(false);
                 var newX = (bb.gameObject.transform.position.x + gameObject.transform.position.x) / 2;
                 var newY = (bb.gameObject.transform.position.y + gameObject.transform.position.y) / 2;
                 var newBlock = Instantiate(blockPrefab, new Vector3(newX, newY, 0), Quaternion.identity);
-        Debug.Log("setting new block");
+                //Debug.Log("setting new block");
                 newBlock.GetComponent<BlockBehavior>().BlockSize = BlockSize + 1;
                 Destroy(bb.gameObject);
                 Destroy(gameObject);
             }
         }
 
+        if (UntouchedBlock)
+        {
+            UntouchedBlock = false;
+            gameManager.SendMessage("BlockLanded");
+        }
     }
 }
